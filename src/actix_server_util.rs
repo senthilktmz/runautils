312816e5a42c63ee79373afb96fef0213b803e37
Senthil Kumar T <senthilktmz@outlook.com>
2024-12-31 00:49:38 +0530

    adding server state store

--- src/actix_server_util.rs
+++ src/actix_server_util.rs
@@ -2,15 +2,23 @@
 use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::any::Any;
+use std::collections::HashMap;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 #[derive(Clone)]
-pub struct ServerContext {
-    pub work_dir: String,
-    pub port: String,
-    pub dependencies: Arc<Box<dyn Any + Send + Sync>>,
+pub struct ServerStateStore {
+    //pub state: HashMap<String, Arc<Box<dyn Any + Send + Sync>>>,//HashMap<String, String>,
+    pub state: Arc<HashMap<String, Arc<Box<dyn Any + Send + Sync>>>>,//HashMap<String, String>,
+}
+
+impl ServerStateStore {
+    pub fn new() -> Self {
+        Self {
+            state: Arc::new(HashMap::new()),
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -22,6 +30,7 @@ pub struct Route {
             web::Json<String>,
             &'static str,
             Arc<Box<dyn Any + Send + Sync>>,
+            Arc<Mutex<ServerStateStore>>,
         ) -> Pin<Box<dyn Future<Output = HttpResponse>>>,
     >,
     pub websocket_handler: Option<
@@ -32,7 +41,8 @@ pub struct Route {
     >,
 }
 
-pub async fn serve_requests (
+
+pub async fn serve_requests(
     routes_list: Vec<Route>,
     tmp_work_dir: String,
     tmp_port: String,
@@ -40,11 +50,16 @@ pub async fn serve_requests (
 ) -> std::io::Result<()> {
     let host_addr = format!("127.0.0.1:{}", tmp_port);
 
+    // Create the shared server state store
+    let server_state_store = Arc::new(Mutex::new(ServerStateStore::new()));
+
     println!("Starting server and serving on http://{}", host_addr);
 
     HttpServer::new(move || {
         let routes_list = routes_list.clone();
         let server_context = server_context.clone();
+        let server_state_store = server_state_store.clone(); // Clone Arc for each thread
+
         let cors = Cors::default()
             .allow_any_origin()
             .allowed_methods(vec!["GET", "POST", "OPTIONS"])
@@ -54,18 +69,25 @@ pub async fn serve_requests (
             ])
             .max_age(3600);
 
-        App::new().wrap(cors).configure(|cfg| {
-            for route in routes_list {
+        App::new().wrap(cors).configure(move |cfg| {
+            for route in routes_list.clone() {
                 if let Some(get_handler) = route.get_handler {
                     cfg.service(web::resource(route.path).route(web::get().to(get_handler)));
                 }
 
                 if let Some(post_handler) = route.post_handler {
                     let server_context = server_context.clone();
+                    let server_state_store = server_state_store.clone();
+
                     cfg.service(web::resource(route.path).route(web::post().to(
                         move |body: web::Json<String>| {
                             let server_context = server_context.clone();
-                            async move { post_handler(body, route.path, server_context).await }
+                            let server_state_store = server_state_store.clone();
+
+                            async move {
+                                post_handler(body, route.path, server_context, server_state_store)
+                                    .await
+                            }
                         },
                     )));
                 }
@@ -76,7 +98,7 @@ pub async fn serve_requests (
             }
         })
     })
-    .bind(host_addr)?
-    .run()
-    .await
+        .bind(host_addr)?
+        .run()
+        .await
 }
